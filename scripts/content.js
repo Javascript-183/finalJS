370483e0f8a58421ea16519bc820d2670a17f437
AKAGEGE87 <akagege87@gmail.com>
2025-02-01 20:50:43 +0400

    content

---
+++ scripts/content.js
@@ -0,0 +1,83 @@
+document.getElementById("readMoreBtn").addEventListener("click", function () {
+    let fullText = document.getElementById("fullText");
+    let shortText = document.getElementById("shortText");
+    let btn = document.getElementById("readMoreBtn");
+
+    if (fullText.style.display === "block") {
+        fullText.style.display = "none";
+        shortText.style.display = "block";
+        btn.innerText = "Read Less «";
+    } else {
+        fullText.style.display = "block";
+        shortText.style.display = "none";
+        btn.innerText = "Read More »";
+    }
+});
+
+
+document.getElementById("readMoreBtnMcxeta").addEventListener("click", function () {
+    let fullText = document.getElementById("fullTextMcxeta");
+    let shortText = document.getElementById("shortTextMcxeta");
+    let btn = document.getElementById("readMoreBtnMcxeta");
+
+    if (fullText.style.display === "block") {
+        fullText.style.display = "none";
+        shortText.style.display = "block";
+        btn.innerText = "Read Less «";
+    } else {
+        fullText.style.display = "block";
+        shortText.style.display = "none";
+        btn.innerText = "Read More »";
+    }
+});
+
+document.getElementById("readMoreBtnKutaisi").addEventListener("click", function () {
+    let fullText = document.getElementById("fullTextKutaisi");
+    let shortText = document.getElementById("shortTextKutaisi");
+    let btn = document.getElementById("readMoreBtnKutaisi");
+
+    if (fullText.style.display === "block") {
+        fullText.style.display = "none";
+        shortText.style.display = "block";
+        btn.innerText = "Read Less «";
+    } else {
+        fullText.style.display = "block";
+        shortText.style.display = "none";
+        btn.innerText = "Read More »";
+    }
+});
+
+document.getElementById("readMoreBtnTelavi").addEventListener("click", function () {
+    let fullText = document.getElementById("fullTextTelavi");
+    let shortText = document.getElementById("shortTextTelavi");
+    let btn = document.getElementById("readMoreBtnTelavi");
+
+
+    if (fullText.style.display === "block") {
+        fullText.style.display = "none";
+        shortText.style.display = "block";
+        btn.innerText = "Read Less «";
+    } else {
+        fullText.style.display = "block";
+        shortText.style.display = "none";
+        btn.innerText = "Read More »";
+    }
+});
+
+document.getElementById("readMoreBtnBorjomi").addEventListener("click", function () {
+    let fullText = document.getElementById("fullTextBorjomi");
+    let shortText = document.getElementById("shortTextBorjomi");
+    let btn = document.getElementById("readMoreBtnBorjomi");
+
+    if (fullText.style.display === "block") {
+        fullText.style.display = "none";
+        shortText.style.display = "block";
+        btn.innerText = "Read Less «";
+    } else {
+        fullText.style.display = "block";
+        shortText.style.display = "none";
+        btn.innerText = "Read More »";
+    }
+});
+
+
